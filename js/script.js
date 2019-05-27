aab7125d335344719ffaeb3c9e234ec040453b86
sai12999 <31101387+sai12999@users.noreply.github.com>
2019-05-27 14:27:19 +0530

    updated with dynamic card generation

--- js/script.js
+++ js/script.js
@@ -6,6 +6,32 @@ var latitude = null;
 var longitude = null;
 var apikey = 'f37978903560541fbae981732c0e563f';
 
+const createTag = (nameOfTag, tagAttributes, parentTag, content) =>
+{
+    var tag = document.createElement(nameOfTag);
+
+    //setting attributes for the tag.
+
+    if(tagAttributes!=null)
+    for(let attribute in tagAttributes)
+    {
+        tag.setAttribute(attribute, tagAttributes[attribute]);
+    }
+
+    //setting content of the tag.
+
+    if(content!=null)
+    {
+        var node = document.createTextNode(content);
+        tag.appendChild(node);
+    }
+
+    //appending newly created tag to its parent.
+
+    var parent = document.getElementById(parentTag);
+    parent.appendChild(tag);
+}
+
 const setLatitudeLongitudeAndCallUserAction = () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function showPosition(position){
@@ -22,5 +48,32 @@ const userAction = async () => {
   let url = `https://developers.zomato.com/api/v2.1/search?lat=${latitude}&lon=${longitude}&apikey=${apikey}`;
   const response = await fetch(url);
   const myJson = await response.json(); //extract JSON from the http response
-  console.log(myJson);
+  parseJsonAndCreateCards(myJson);
 }
+
+const parseJsonAndCreateCards = (myJson) =>{
+  var parent = 'flex-container';
+  console.log(myJson);
+  //creation of cards.
+  for(var index in myJson.restaurants)
+  {//  console.log(myJson.restaurants[index].restaurant.name);
+  createTag(`div`,{"id":`${index}`,"class":`normalcard`},`${parent}`,null);
+  createTag(`img`,{"src":`${myJson.restaurants[index].restaurant.thumb}`,"alt":`${myJson.restaurants[index].restaurant.name}`},`${index}`,null);
+  createTag(`div`,{"class":"normalcontainer","id":`${index}container`},`${index}`,null);
+  createTag(`h4`,{"id":`${myJson.restaurants[index].restaurant.name}`},`${index}container`,`${myJson.restaurants[index].restaurant.name}`);
+  //creating area for hiding and showing details.
+  
+  }
+}
+
+
+//script for bootstrap nav bar creation.
+createTag(`div`,{"id":`top-header`},`body`,null);
+createTag(`nav`,{"class":`navbar navbar-inverse`,"id":`nav`},`top-header`,null);
+createTag(`div`,{"class":`container`,"id":`container`},`nav`,null);
+createTag(`div`,{"class":`navbar-header`,"id":`navbar-header`},`container`,null);
+createTag(`div`,{"class":`navbar-brand`,"id":`navbar-brand`},`navbar-header`,`Zomato Developer API`);
+createTag(`ul`,{"class":`nav navbar-nav navbar-right`,"id":`navul`},`container`,null);
+createTag(`li`,{"id":`homeli`},`navul`,null);
+createTag(`a`,{"href":"#","id":"homea"},`homeli`,null);
+createTag(`b`,null,`homea`,`HOME`);	
